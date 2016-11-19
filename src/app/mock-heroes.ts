72309ea8d40df75dcb7ba4e218f929d4928a4ef4
Krister <krister.torsvik@gmail.com>
2016-11-19 12:47:46 +0100

    changed hero names

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -6,7 +6,7 @@ export const HEROES: Hero[] = [
   { id: 13, name: 'Mr. Noice' },
   { id: 14, name: 'Brokkoli' },
   { id: 15, name: 'Magneet' },
-  { id: 16, name: 'RubberMan' },
+  { id: 16, name: 'Rubber Duck-Man' },
   { id: 17, name: 'Dynama-sama' },
   { id: 18, name: 'Dr. Brains' },
   { id: 19, name: 'Melted Rocks' },
