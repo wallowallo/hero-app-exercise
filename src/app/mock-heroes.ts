b9fab6dfca6d7dfada642b5ee7c8ead66c4526f3
Krister <krister.torsvik@gmail.com>
2016-11-19 12:47:09 +0100

    changed hero names

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
+  { id: 11, name: 'SuperMayne' },
+  { id: 12, name: 'Bertman' },
+  { id: 13, name: 'Mr. Noice' },
+  { id: 14, name: 'Brokkoli' },
+  { id: 15, name: 'Magneet' },
   { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 17, name: 'Dynama-sama' },
+  { id: 18, name: 'Dr. Brains' },
+  { id: 19, name: 'Melted Rocks' },
+  { id: 20, name: 'Awfully Windy-Man' }
 ];
